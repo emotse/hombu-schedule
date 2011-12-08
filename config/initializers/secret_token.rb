dde4f78353ef4c989206f2fff9277431df46cc41
Eric Pickett <imac_alpha@EricPicket-no-iMac.local>
2011-12-08 12:10:00 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HombuSchedule::Application.config.secret_token = 'e9fa9c17218d89b216466c4dff8d64a0faa928b4c26587680d6e46f4aa0e98c819aee6ec6cd0008a3ee217c13e55fe1d80552e10c4d9fb6f0add89dc3a83b3c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HombuSchedule::Application.config.session_store :cookie_store, key: '_hombu-schedule_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HombuSchedule::Application.config.session_store :active_record_store
