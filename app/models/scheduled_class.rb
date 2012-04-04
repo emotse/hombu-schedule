27b12d84e9b0971a2f079ae93520164052d7712e
Eric Pickett <plangent@gmail.com>
2012-04-04 18:08:32 +0900

    switched calendar view to table

--- app/models/scheduled_class.rb
+++ app/models/scheduled_class.rb
@@ -5,13 +5,12 @@ class ScheduledClass < ActiveRecord::Base
 
   def self.get_days
     days = ScheduledClass.select(:day)
-    days.uniq { |day| day.day }
+    days.uniq! { |day| day.day }
   end
 
   def self.get_times
     times = ScheduledClass.select(:time)
-    times.uniq! { |time| time.time }
-    times.sort { |x, y| x.time <=> y.time }
+    times.uniq { |time| time.time }.sort { |x, y| x.time <=> y.time }
   end
 
   def self.get_by_day_and_time day, time
