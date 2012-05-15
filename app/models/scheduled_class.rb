2491d392a7f81a8d5e0e65863c004c2050057527
Eric Pickett <plangent@gmail.com>
2012-05-15 11:31:25 +0900

    no idea

--- app/models/scheduled_class.rb
+++ app/models/scheduled_class.rb
@@ -14,6 +14,6 @@ class ScheduledClass < ActiveRecord::Base
   end
 
   def self.get_by_day_and_time day, time
-    this_class = ScheduledClass.where({:day => day, :time => time})
+    this_class = ScheduledClass.where {:day => day, :time => time}
   end
 end
