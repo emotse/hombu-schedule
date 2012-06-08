f7970e6dfd6b01e8e2870dc3573caa1877163eee
Eric Pickett <plangent@gmail.com>
2012-06-08 18:00:12 +0900

    urgh

--- lib/mechanize.rb
+++ lib/mechanize.rb
@@ -10,4 +10,8 @@ page = agent.post(url, 'FormDate' => requested_date)
 
 items = page.search('td:nth-child(2) td')
 
-puts items
+items.each do |item|
+  item_text = item.text()
+  item_time = item_text.match(/^\d+:\d+-\d+:\d+/)
+  item_teacher = item_text.match(/\w*\.?\w+$/)
+end
