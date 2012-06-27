fd941d307fec637ebaf11842fc97ec78062249fb
emotse <plangent@gmail.com>
2012-06-27 21:16:10 +0900

    switched dev to postgres

--- db/migrate/20120312171633_add_day_to_scheduled_classes.rb
+++ db/migrate/20120312171633_add_day_to_scheduled_classes.rb
@@ -1,7 +1,6 @@
 class AddDayToScheduledClasses < ActiveRecord::Migration
   def change
     remove_column :scheduled_classes, :time
-    remove_column :scheduled_classes, :day
     add_column :scheduled_classes, :day, :string
     add_column :scheduled_classes, :time, :string
   end
